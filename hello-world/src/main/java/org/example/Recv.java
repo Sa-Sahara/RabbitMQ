15f18f1df4efb5d6bd1b1883c41b114aea79da12
Aleksandra Saharova <privet49069@gmail.com>
2023-11-04 00:29:37 +0600

    add instructions how to compile&run from CMD

--- hello-world/src/main/java/org/example/Recv.java
+++ hello-world/src/main/java/org/example/Recv.java
@@ -5,7 +5,23 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/*
+ * Make sure docker container with RabbitMQ is running:
+ * docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
+ *
+ * external libs saved to:
+ * C:\Users\user\IdeaProjects\coursera\RabbitMQ\hello-world\src\main\java
+ *
+ * cd C:\Users\user\IdeaProjects\coursera\RabbitMQ\hello-world\src\main\java\org\example
+ *
+ * compile both classes if not yet:
+ * javac -cp amqp-client-5.16.0.jar Send.java Recv.java
+ *
+ * run Recv
+ * java -cp .;amqp-client-5.16.0.jar;slf4j-api-1.7.36.jar;slf4j-simple-1.7.36.jar org.example.Recv
+ * */
 
 public class Recv {
     private final static String QUEUE_NAME = "hello";
@@ -20,7 +36,7 @@ public class Recv {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println(" [x] Received '" + message + "'");
         };
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
--- hello-world/src/main/java/org/example/Send.java
+++ hello-world/src/main/java/org/example/Send.java
@@ -7,6 +7,22 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/*
+ * Make sure docker container with RabbitMQ is running:
+ * docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
+ *
+ * external libs saved to:
+ * C:\Users\user\IdeaProjects\coursera\RabbitMQ\hello-world\src\main\java
+ *
+ * cd C:\Users\user\IdeaProjects\coursera\RabbitMQ\hello-world\src\main\java\org\example
+ *
+ * compile both classes if not yet:
+ * javac -cp amqp-client-5.16.0.jar Send.java Recv.java
+ *
+ * run Send
+ * java -cp .;amqp-client-5.16.0.jar;slf4j-api-1.7.36.jar;slf4j-simple-1.7.36.jar org.example.Send
+ * */
+
 public class Send {
     private final static String QUEUE_NAME = "hello";
 
