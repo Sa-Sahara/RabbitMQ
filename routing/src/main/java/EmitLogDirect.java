955331196895fb9cbe7adc72207263d5aee7da97
Aleksandra Saharova <privet49069@gmail.com>
2023-11-04 21:51:52 +0600

    send/receive messages by direct exchange, severity - 'info', 'warning', 'error'

---
+++ routing/src/main/java/EmitLogDirect.java
@@ -0,0 +1,61 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Implement filtering messages based on their severity
+ * 'severity' can be one of 'info', 'warning', 'error'.
+ * Earlier used fanout exchange - mindless broadcasting.
+ * direct exchange - message goes to the queues whose
+ * binding key == routing key of the message.
+ * <p>
+ * In this setup, we can see the direct exchange X with
+ * two queues bound to it.
+ * The first queue is bound with binding key orange,
+ * and the second has two bindings - black and green.
+ */
+public class EmitLogDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+            String severity = getSeverity(argv);
+            String message = getMessage(argv);
+
+            channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+        }
+    }
+
+    private static String getSeverity(String[] strings) {
+        if (strings.length < 1)
+            return "info";
+        return strings[0];
+    }
+
+    private static String getMessage(String[] strings) {
+        if (strings.length < 2)
+            return "Hello World!";
+        return joinStrings(strings, " ", 1);
+    }
+
+    private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+        int length = strings.length;
+        if (length == 0) return "";
+        if (length <= startIndex) return "";
+        StringBuilder words = new StringBuilder(strings[startIndex]);
+        for (int i = startIndex + 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
+    }
+}
